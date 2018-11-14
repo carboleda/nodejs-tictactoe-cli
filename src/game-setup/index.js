a9d89c8cc171e153bb5e1e5696186355ce46c2c9
Carlos Fernando Arboleda Garces <arbofercho@gmail.com>
2018-11-13 22:40:11 -0500

    Se adiciona validacion de las respuestas en el game-setup

--- src/game-setup/index.js
+++ src/game-setup/index.js
@@ -12,10 +12,10 @@ module.exports = function (socket, options) {
     socket.on('receive available matches', selectMatch);
 
     async function init() {
-        SEPTUP.nickName = await makeQuestion('Nickname:');
-        SEPTUP.createOrJoin = await makeQuestion(`${SEPTUP.nickName}, what do you want to do?`, [
+        SEPTUP.nickName = await makeQuestion('Nickname: ');
+        SEPTUP.createOrJoin = await makeQuestion(`${SEPTUP.nickName}, what do you want to do?: `, [
             '1. Create new match',
-            '2. Join a game'
+            '2. Join a match'
         ]);
         if(SEPTUP.createOrJoin == '1') {
             socket.emit('new match', SEPTUP);
@@ -28,7 +28,7 @@ module.exports = function (socket, options) {
     async function selectMatch(matches) {
         if(matches.length > 0) {
             const matchesChooise = matches.map((match, index) => `${index + 1}. ${match}`);
-            const matchIndex = await makeQuestion(`Select a match:`, matchesChooise);
+            const matchIndex = await makeQuestion('Select a match: ', matchesChooise);
             SEPTUP.matchName = matches[+matchIndex - 1];
             socket.emit('join to match', SEPTUP);
             options.onFinish();
@@ -46,11 +46,29 @@ module.exports = function (socket, options) {
 
 function makeQuestion(question, choices = []) {
     return new Promise((resolve, reject) => {
-        Utilities.clearScreen();
+        const hasChoices = choices.length > 0;
         const breakLine = choices.length > 0 ? '\n' : ' ';
-        const questionAndChoices = `${question}${breakLine}${choices.join('\n')}${breakLine}`;
-        rl.question(questionAndChoices, answer => {
-            resolve(answer);
-        });
+        const questionAndChoices = `${choices.join('\n')}${breakLine}${question}`;
+
+        //Se hace la pregunta
+        function onQuestion() {
+            Utilities.clearScreen();
+            rl.question(questionAndChoices, onAnswer);
+        }
+
+        function onAnswer(answer) {
+            //console.log('makeQuestion', answer);
+            //Si la pregunta NO tiene opciones o tienes la respuesta es valida
+            if(!hasChoices
+                || (hasChoices && +answer >= 1 && +answer <= choices.length)) {
+                    //Se resuelve la promesa enviando la resputa
+                resolve(answer);
+            } else {
+                //De lo contrario se vuelve a preguntar hasta obtener una respuesta correcta
+                onQuestion(question, choices);
+            }
+        }
+
+        onQuestion();
     });
 }
